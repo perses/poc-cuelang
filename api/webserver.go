165c88f7f4e278a5db8ef9b775ce1f0ec8c0d1b4
AntoineThebaud <antoine.thebaud@yahoo.fr>
2022-03-24 17:25:27 +0100

    cuelang compilation, evaluation and decoding into go struct for Datasource objects
    Signed-off-by: AntoineThebaud <antoine.thebaud@yahoo.fr>

--- api/webserver.go
+++ api/webserver.go
@@ -6,30 +6,96 @@ import (
 
 	"cuelang.org/go/cue"
 	"cuelang.org/go/cue/cuecontext"
+	"cuelang.org/go/cue/errors"
 	"github.com/labstack/echo/v4"
 	utilsEcho "github.com/perses/common/echo"
+	persesModel "github.com/perses/perses/pkg/model/api/v1"
+)
+
+const (
+	schemaName = "datasourceSchema"
+	schemaDef  = "#" + schemaName + `: {
+		kind: "Datasource"
+		metadata: {
+			name: string
+		}
+		spec: {
+			kind: string | *"Prometheus"
+			default: bool
+			http: {
+				url: string
+			}
+		}
+	}
+	`
 )
 
 type ServerAPI struct {
 	utilsEcho.Register
+	ctx    *cue.Context
+	schema cue.Value
 }
 
 func NewServerAPI() *ServerAPI {
-	return &ServerAPI{}
+	// create a Context
+	ctx := cuecontext.New()
+	// compile our schema
+	schema := ctx.CompileString(schemaDef)
+
+	return &ServerAPI{
+		ctx:    ctx,
+		schema: schema,
+	}
 }
 
 func (s *ServerAPI) RegisterRoute(e *echo.Echo) {
 	e.POST("/validate", func(c echo.Context) error {
 		data, err := ioutil.ReadAll(c.Request().Body)
 
-		// create a Context
-		ctx := cuecontext.New()
+		fmt.Println("User input :")
+		fmt.Println(string(data))
 
-		// compile the input into a Value  (= cue evaluation)
-		var v cue.Value = ctx.CompileBytes(data)
+		// build the final CUE payload as a combination of user input & schema constraint
+		cueData := fmt.Sprintf("#%s & %s", schemaName, data)
 
-		// print the value
-		fmt.Println(v)
+		// compile the CUE data into a Value, with scope
+		v := s.ctx.CompileString(cueData, cue.Scope(s.schema))
+
+		// check for errors during compiling
+		if v.Err() != nil {
+			msg := errors.Details(v.Err(), nil)
+			fmt.Printf("Compile Error:\n%s\n", msg)
+		} else {
+			fmt.Println("CUE compilation result :")
+			fmt.Println(v)
+		}
+
+		// evaluate the CUE Value
+		e := v.Eval()
+		if e.Err() != nil {
+			msg := errors.Details(e.Err(), nil)
+			fmt.Printf("Eval Error:\n%s\n", msg)
+		} else {
+			fmt.Println("CUE evaluation result :")
+			fmt.Printf("%#v\n", e)
+		}
+
+		// check if the CUE Value is concrete
+		if v.IsConcrete() {
+			fmt.Println("CUE Value is concrete")
+		} else {
+			fmt.Println("CUE Value is not concrete")
+		}
+
+		// decode the CUE Value into Go struct
+		var datasource persesModel.Datasource
+		decodeErr := v.Decode(&datasource)
+		if decodeErr != nil {
+			fmt.Printf("Decode Error:\n%s\n", decodeErr)
+		} else {
+			fmt.Println("CUE Value successfully decoded into a Datasource object :")
+			fmt.Println(datasource)
+		}
 
 		return err
 	})
--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ require (
 	cuelang.org/go v0.4.2
 	github.com/labstack/echo/v4 v4.7.2
 	github.com/perses/common v0.10.0
-	github.com/sirupsen/logrus v1.8.1
+	github.com/perses/perses v0.0.0-20220323190413-5567c3dc05e1
 )
